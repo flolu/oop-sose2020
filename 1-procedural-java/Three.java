7ebd07b0a2b64433b9bd7970a610e3e932d8d049
flolu <fllu@pm.me>
2020-05-15 21:20:00 +0200

    finish(1): 3, recursive "prost" counter

---
+++ 1-procedural-java/Three.java
@@ -0,0 +1,13 @@
+public class Three {
+  public static void main(String[] args) {
+    int n = Integer.parseInt(args[0]);
+    int prosts = prost(n);
+    System.out.println(prosts);
+  }
+
+  public static int prost(int n) {
+    if (n == 1)
+      return 1;
+    return n + prost(n - 1);
+  }
+}
